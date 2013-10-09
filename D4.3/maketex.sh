494e86791a872cbb1202cdb3db6af766f4cc261b
Graham Klyne <gk-github@ninebynine.org>
2013-10-09 15:21:10 +0100

    Implemented Markdown conversion to PDF via LaTeX, using LateX templates from D2.2v2.  Using BibTeX references.  Fixed some typos and improved prose.

--- D4.3/maketex.sh
+++ D4.3/maketex.sh
@@ -1,3 +1,11 @@
+# Convert markdown to .tex for document body
 pandoc D4.3-draft.md -f markdown -t latex --table-of-contents --toc-depth=4 -o D4.3-body.tex
+# Generate .aux
 pdflatex D4.3.tex
+# Generate .bbl
+bibtex D4.3
+# Page count
 pdflatex D4.3.tex
+# Final formatting
+pdflatex D4.3.tex
+# End.
