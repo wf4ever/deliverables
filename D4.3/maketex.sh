01acfaf5e0a5e655abbf08e49ba7651551a214b1
Graham Klyne <gk-github@ninebynine.org>
2013-11-04 12:33:41 +0000

    Merge stability text into section 4 markdown file (no images)

--- D4.3/maketex.sh
+++ D4.3/maketex.sh
@@ -1,5 +1,6 @@
 # Convert markdown to .tex for document body
 pandoc D4.3-draft.md -f markdown -t latex --table-of-contents --toc-depth=4 -o D4.3-body.tex
+# pandoc D4.3-section-4-stability -f markdown -t latex --table-of-contents --toc-depth=4 -o D4.3-section-4-stability.tex
 # Generate .aux
 pdflatex D4.3.tex
 # Generate .bbl
